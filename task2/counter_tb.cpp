20c625d5ac6568ea7111f268f068b843d11c78d4
Idrees <33992400+idrees-mahmood@users.noreply.github.com>
2023-11-01 19:29:10 +0000

    Progres on task 2

--- task2/counter_tb.cpp
+++ task2/counter_tb.cpp
@@ -1,6 +1,6 @@
 #include "Vcounter.h"
 #include "verilated.h"
-#include <verilated_vcd_c.h>
+#include "verilated_vcd_c.h"
 #include "vbuddy.cpp"
 
 
@@ -27,30 +27,31 @@ int main(int argc, char **argv, char **env)
     top->rst = 1;
     top->en = 0;
 
-    bool count_pause = false;
-    // run simulation for many clock cycles
-    for (i = 0; i < 300; i++)
-    {
+    // run simulation for 300 clock periods
 
-        // dump variables into VCD file and toggle clock
-        for (clk = 0; clk < 2; clk++)
-        {
-            tfp->dump(2 * i + clk);
-            if (i < 9 || i > 12)
-            {
-                top->clk = !top->clk;
-            }
-            else
-            {
-                top->clk = top->clk;
-            }
+    for (i = 0; i<300; i++) {
+
+        //dump variables into VCD file and toggle clock
+        for (clk = 0; clk < 2; clk++) {
+            tfp->dump(2*i+clk); // unit is in ps (periods)
+            top->clk = !top->clk;
             top->eval();
         }
-        top->rst = (i < 2) | (i == 15);
-        top->en = (i > 4);
-        if (Verilated::gotFinish())
-            exit(0);
+
+        // ++++ Send count value to vBuddy
+        vbdHex(4, (int(top->count) >> 16) * 0xF);
+        vbdHex(3, (int(top->count) >> 8) * 0xF);
+        vbdHex(2, (int(top->count) >> 4) * 0xF);
+        vbdHex(1, int(top->count) * 0xF);
+        // ---- End of VBuddy output section
+
+        // change input stimuli
+        top -> rst = (i < 2) | (i == 15);
+        top -> en = vbdFlag();
+        if (Verilated::gotFinish())  exit(0);
     }
+
+    vbdClose(); // close vBuddy prevents memory leaks
     tfp->close();
     exit(0);
 }
